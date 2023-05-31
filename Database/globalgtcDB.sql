4dc2382b49b9e2fbf20133e3ab67ebeeea5766d5
Andres Sepulveda <bodyweightforce@gmail.com>
2023-05-30 22:44:06 -0400

    Create_at 30/05/2023

---
+++ Database/globalgtcDB.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS globalgtc;
+CREATE DATABASE IF NOT EXISTS globalgtc;
+USE globalgtc;
+
+CREATE TABLE cotizacion(
+id INT PRIMARY KEY UNIQUE NOT NULL,
+vendedor INT NOT NULL,
+cliente INT NOT NULL,
+fecha DATE NOT NULL,
+detalle_cotizacion INT UNIQUE NOT NULL
+);
+CREATE TABLE vendedor(
+id INT PRIMARY KEY UNIQUE NOT NULL,
+nombre VARCHAR(25) NOT NULL
+);
+CREATE TABLE cliente(
+id INT PRIMARY KEY UNIQUE NOT NULL,
+nombre VARCHAR(25) NOT NULL,
+rut VARCHAR(25),
+direccion VARCHAR(25),
+telefono VARCHAR(25),
+correo_electronico VARCHAR(25) NOT NULL
+);
+CREATE TABLE detalle_cotizacion(
+id INT PRIMARY KEY UNIQUE NOT NULL,
+productos INT NOT NULL
+);
+CREATE TABLE producto(
+id INT PRIMARY KEY UNIQUE NOT NULL,
+nombre VARCHAR(45) NOT NULL UNIQUE,
+precio INT NOT NULL,
+peso INT NOT NULL,
+medidas VARCHAR(15) NOT NULL
+);
+
