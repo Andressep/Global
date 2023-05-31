5c86058ffabddd03216d47f267a39906c3d3198d
Andres Sepulveda <bodyweightforce@gmail.com>
2023-05-30 23:35:28 -0400

    Relacion entre tablas

--- Database/globalgtcDB.sql
+++ Database/globalgtcDB.sql
@@ -7,7 +7,10 @@ id INT PRIMARY KEY UNIQUE NOT NULL,
 vendedor INT NOT NULL,
 cliente INT NOT NULL,
 fecha DATE NOT NULL,
-detalle_cotizacion INT UNIQUE NOT NULL
+detalle_cotizacion INT UNIQUE NOT NULL,
+FOREIGN KEY (cliente) REFERENCES cliente(id),
+FOREIGN KEY (vendedor) REFERENCES vendedor(id),
+FOREIGN KEY (detalle_cotizacion) REFERENCES detalle_cotizacion(id)
 );
 CREATE TABLE vendedor(
 id INT PRIMARY KEY UNIQUE NOT NULL,
@@ -32,4 +35,7 @@ precio INT NOT NULL,
 peso INT NOT NULL,
 medidas VARCHAR(15) NOT NULL
 );
-
+ALTER TABLE cotizacion
+ADD FOREIGN KEY (cliente) REFERENCES cliente(id),
+ADD FOREIGN KEY (vendedor) REFERENCES vendedor(id),
+ADD FOREIGN KEY (detalle_cotizacion) REFERENCES detalle_cotizacion(id);
